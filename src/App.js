36e24a2cc1f05cbc123d20fa088e1bb0390dae27
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-03-04 10:03:36 +0200

    Submitting & logging

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+          }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+        {
+          title: 'Business website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social app',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Shopping Card',
+          category: 'Web development'
+        }
+      ]})
+  }
+handleAddProject(project){
+    console.log(project);
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
